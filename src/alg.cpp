896e53cb718488e0fb0ea8242274eeb2d3524693
Матвеев Артем <99340742+MATVEEVru@users.noreply.github.com>
2022-06-10 21:30:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,3 +1,4 @@
+// Copyright 2021 NNTU-CS
 #include  <iostream>
 #include  <fstream>
 #include  <locale>
@@ -5,30 +6,30 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
- BST<std::string> Drevo;
- std::ifstream fin(filename);
- char tempr;
- std::string word = "";
- if (!fin.is_open()) {
-     std::cout << "Error! File can't be open!" << std::endl;
-     return Drevo;
- }
- while (!fin.eof()) {
-     tempr = fin.get();
-     if (tempr >= 'A') {
-         if (tempr <= 'Z') {
-             tempr = tempr + 32;
-         }
-     }
-     if (tempr >= 'a') {
-         if (tempr <= 'z') {
-             word = word + tempr;
-         }
-     } else {
-         Drevo.Add(word);
-         word = "";
-     }
- }
- fin.close();
- return Drevo;
+  BST<std::string> Drevo;
+  std::ifstream fin(filename);
+  char tempr;
+  std::string word = "";
+  if (!fin.is_open()) {
+      std::cout << "Error! File can't be open!" << std::endl;
+      return Drevo;
+  }
+  while (!fin.eof()) {
+      tempr = fin.get();
+      if (tempr >= 'A') {
+          if (tempr <= 'Z') {
+              tempr = tempr + 32;
+          }
+      }
+      if (tempr >= 'a') {
+          if (tempr <= 'z') {
+              word = word + tempr;
+          }
+      } else {
+          Drevo.Add(word);
+          word = "";
+      }
+  }
+  fin.close();
+  return Drevo;
 }
